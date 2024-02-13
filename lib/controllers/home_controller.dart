be556ea62d1bb3450534288b19e56dd977536ee5
boiiloat <boiiloat168@gmail.com>
2024-02-13 11:39:22 +0700

    separate component

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:housekeeping/services/api_service.dart';
@@ -32,4 +31,5 @@ class HomeController extends GetxController {
   Future<void> onRefresh() async {
     await _onInitState;
   }
+  
 }
